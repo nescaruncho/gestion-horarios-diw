1169b305fa28f96cace31d51d5c189299be34bf5
a23carolinabc <148218357+a23carolinabc@users.noreply.github.com>
2025-03-13 18:51:51 +0100

    Add files via upload

---
+++ APP-HORARIOS/gestionaModulo.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header("Location: login.php");
+    exit();
+}
+require_once "conexion.php";
+
+try {
+    switch ($_POST["boton"]) {
+        case 'editar':
+            $pdoStatement=$pdo->prepare("UPDATE modulo SET name=?, curso=?, horas_totales=? WHERE id_modulo=?");
+            $pdoStatement->bindParam(1, $_POST['nombreModulo']);
+            $pdoStatement->bindParam(2, $_POST["cursoModulo"]);
+            $pdoStatement->bindParam(3, $_POST["horasModulo"]);
+            $pdoStatement->bindParam(4, $_POST['idModulo']);
+            break;
+    
+        case 'eliminar':
+            $pdoStatement=$pdo->prepare("DELETE FROM modulo WHERE id_modulo=?;");
+            $pdoStatement->bindParam(1, $_POST['idModulo']);
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+
+    $pdoStatement->execute();
+
+    header("Location: admin.php");
+    exit();
+
+} catch (PDOException $e) {
+    $errorInfo = $e->errorInfo;
+    $mensaje = "Error: " . $e->getMessage();
+    
+    $_SESSION['error'] = $mensaje;
+    header("Location: admin.php");
+    exit();
+
+}
---
+++ APP-HORARIOS/validaCiclo.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header("Location: login.php");
+    exit();
+}
+
+if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+    $_SESSION['error'] = "Error: la validación CSRF ha fallado. Por favor, inténtelo de nuevo.";
+    header("Location: login.php");
+    exit();
+}
+
+require_once "conexion.php";
+
+try {
+    $pdoStatement=$pdo->prepare("INSERT INTO ciclo (codigo, name) VALUES (?,?)");
+    $codigo=$_POST["codigoCiclo"];
+    $nombre=$_POST["nombreCiclo"];
+    
+    $pdoStatement->bindParam(1, $codigo);
+    $pdoStatement->bindParam(2, $nombre);
+    $pdoStatement->execute();
+
+    unset($_SESSION['csrf_token']);
+
+    header("Location: admin.php");
+    exit();
+
+} catch (PDOException $e) {
+    $errorInfo = $e->errorInfo;
+    if (isset($errorInfo[1]) && $errorInfo[1] == 1062) {
+        $mensaje = "Error: El nombre de usuario o el email ya están en uso.";
+    } else {
+        $mensaje = "Error: " . $e->getMessage();
+    }
+
+    unset($_SESSION['csrf_token']);
+    
+    $_SESSION['error'] = $mensaje;
+    header("Location: admin.php");
+    exit();
+
+}
